ecee5575b750185a9e8712228b6f6d3bc0578409
CodingGuyJW <juice0914@hanmail.net>
2021-08-05 18:17:06 +0900

    2021-08-05

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('header').load('inc.html header > div')
+    $('.menu').load('inc.html .menu > div')
+    $('.left').load('inc.html .left > div')
+    $('.right').load('inc.html .right > div')
+    $('footer').load('inc.html footer > .foot-cont')
+})
